31a0d308583cfa24a874041c163159e1e64fb731
codethi <docencia.thiago@gmail.com>
2022-11-11 21:08:47 -0300

    feat: sign-up and basic sign-in and bcrypt

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient } from "mongodb";
 import cors from "cors";
 import dotenv from "dotenv";
 import joi from "joi";
+import bcrypt from "bcrypt";
 
 const userSchema = joi.object({
   name: joi.string().required().min(3).max(100),
@@ -26,7 +27,55 @@ try {
 const db = mongoClient.db("authDb");
 const userCollection = db.collection("users");
 
+// ROTAS
+app.post("/sign-up", async (req, res) => {
+  const user = req.body;
 
+  try {
+    const userExists = await userCollection.findOne({ email: user.email });
+    if (userExists) {
+      return res.status(409).send({ message: "Esse email já existe" });
+    }
+
+    const { error } = userSchema.validate(user, { abortEarly: false });
+
+    if (error) {
+      const errors = error.details.map((detail) => detail.message);
+      return res.status(400).send(errors);
+    }
+
+    const hashPassword = bcrypt.hashSync(user.password, 10);
+
+    await userCollection.insertOne({ ...user, password: hashPassword });
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userExists = await userCollection.findOne({ email });
+    
+    if (!userExists) {
+      return res.sendStatus(401);
+    }
+
+    const passwordOk = bcrypt.compareSync(password, userExists.password);
+
+    if (!passwordOk) {
+      return res.sendStatus(401);
+    }
+
+    res.send({ message: `Olá ${userExists.name}, seja bem vindo(a)!` });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 
 app.listen(4000, () => {
   console.log(`Server running in port: ${4000}`);
